3b73432f0d2c83c6ce6e67e2a5be31005ee25a49
erri120 <erri120@fossmailer.de>
2025-11-05 16:07:14 +0100

    Use scanning migration

--- src/NexusMods.DataModel.SchemaVersions/Migrations/_0010_ChangeGameMetadata.cs
+++ src/NexusMods.DataModel.SchemaVersions/Migrations/_0010_ChangeGameMetadata.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.Logging;
 using NexusMods.Abstractions.GameLocators;
 using NexusMods.MnemonicDB.Abstractions;
 using NexusMods.MnemonicDB.Abstractions.ElementComparers;
+using NexusMods.MnemonicDB.Abstractions.Internals;
 using NexusMods.MnemonicDB.Abstractions.Query;
 using NexusMods.MnemonicDB.Abstractions.Query.SliceDescriptors;
 using NexusMods.MnemonicDB.Abstractions.ValueSerializers;
@@ -15,7 +16,7 @@ using NexusMods.Sdk.NexusModsApi;
 
 namespace NexusMods.DataModel.SchemaVersions.Migrations;
 
-public class _0010_ChangeGameMetadata : ITransactionalMigration
+public class _0010_ChangeGameMetadata : IScanningMigration
 {
     public static (MigrationId Id, string Name) IdAndName { get; } = MigrationId.ParseNameAndId(nameof(_0010_ChangeGameMetadata));
 
@@ -61,69 +62,39 @@ public class _0010_ChangeGameMetadata : ITransactionalMigration
         return Task.CompletedTask;
     }
 
-    public void Migrate(ITransaction tx, IDb db)
+    public ScanResultType Update(ref KeyPrefix prefix, ReadOnlySpan<byte> valueSpan, in IBufferWriter<byte> writer)
     {
-        MigrateGameId(tx, db);
-        MigrateName(tx, db);
-    }
+        if (prefix.A == _nameAttributeId) return ScanResultType.Delete;
+        if (prefix.A != _gameIdAttributeId) return ScanResultType.None;
 
-    private void MigrateName(ITransaction tx, IDb db)
-    {
-        if (!_nameAttributeId.HasValue)
+        if (prefix.ValueTag is ValueTag.UInt32)
         {
-            _logger.LogInformation("Skipping name migration, found no matching attribute");
-            return;
-        }
-
-        var datoms = db.Datoms(new AttributeSlice(_nameAttributeId.Value));
-        if (datoms.Count == 0)
-        {
-            _logger.LogInformation("Skipping name migration, found no datoms to migrate");
-            return;
-        }
-
-        foreach (var datom in datoms)
-        {
-            tx.Add(datom.Retract());
-        }
-    }
-
-    private void MigrateGameId(ITransaction tx, IDb db)
-    {
-        if (!_gameIdAttributeId.HasValue)
-        {
-            _logger.LogInformation("Skipping game id migration, found no matching attribute");
-            return;
-        }
-
-        var datoms = db.Datoms(new AttributeSlice(_gameIdAttributeId.Value));
-        if (datoms.Count == 0)
-        {
-            _logger.LogInformation("Skipping game id migration, found no datoms to migrate");
-            return;
-        }
-
-        var writer = new ArrayBufferWriter<byte>(initialCapacity: sizeof(ulong));
-
-        foreach (var datom in datoms)
-        {
-            if (datom.Prefix.ValueTag is not ValueTag.UInt32)
-            {
-                _logger.LogWarning("Datom considered for migration has value tag {Actual} instead of {Expected}", datom.Prefix.ValueTag, ValueTag.UInt32);
-                continue;
-            }
-
-            var nexusModsGameId = NexusModsGameId.From(UInt32Serializer.Read(datom.ValueSpan));
+            var nexusModsGameId = NexusModsGameId.From(UInt32Serializer.Read(valueSpan));
             if (!_idMappings.TryGetValue(nexusModsGameId, out var gameId))
             {
-                _logger.LogWarning("Unable to migrate datom due to missing mapping for Nexus Mods game id {NexusModsGameId}", nexusModsGameId);
-                tx.Add(datom.Retract());
-                continue;
+                _logger.LogWarning("Unable to migrate datom {E} due to missing mapping for Nexus Mods game id {NexusModsGameId}", prefix.E, nexusModsGameId);
+                return ScanResultType.None;
             }
 
             UInt64Serializer.Write(gameId.Value, writer);
-            tx.Add(datom.E, datom.A, ValueTag.UInt64, writer.WrittenSpan);
-            writer.Clear();
+            return ScanResultType.Update;
+        } else if (prefix.ValueTag is ValueTag.UInt64)
+        {
+            var value = UInt64Serializer.Read(valueSpan);
+            var gameId = GameId.From(value);
+
+            if (!_idMappings.TryGetFirst(kv => kv.Value == gameId, out var pair))
+            {
+                _logger.LogWarning("Unknown game id `{GameId}` while scanning for migration", gameId);
+                return ScanResultType.None;
+            }
+
+            return ScanResultType.None;
+        }
+        else
+        {
+            _logger.LogWarning("Unable to migrate datom {E} due to unsupported value tag {ValueTag}", prefix.E, prefix.ValueTag);
+            return ScanResultType.None;
         }
     }
 }
--- tests/NexusMods.DataModel.SchemaVersions.Tests/MigrationSpecificTests/TestsFor_0010_ChangeGameMetadata.cs
+++ tests/NexusMods.DataModel.SchemaVersions.Tests/MigrationSpecificTests/TestsFor_0010_ChangeGameMetadata.cs
@@ -1,6 +1,7 @@
 using FluentAssertions;
 using NexusMods.Abstractions.Loadouts;
 using NexusMods.MnemonicDB.Abstractions;
+using NexusMods.MnemonicDB.Abstractions.Query;
 using NexusMods.Sdk.Games;
 using Xunit.Abstractions;
 
@@ -18,16 +19,11 @@ public class TestsFor_0010_ChangeGameMetadata(ITestOutputHelper helper) : ALegac
         connection.TxId.Value.Should().Be(connection.DatomStore.AsOfTxId.Value);
         // connection.Db.BasisTxId.Value.Should().Be(connection.TxId.Value); // fails because DB hasn't been updated
 
-        // var db = connection.Db;
-        // db.BasisTxId.Value.Should().NotBe(72057594037927971); // fails because DB hasn't been updated
-
-        // TODO: connection is one DB revision behind
-        var db = connection.AsOf(connection.TxId);
-        db.BasisTxId.Value.Should().NotBe(72057594037927971);
+        var db = connection.Db;
 
         // NOTE(erri120): because of our testing framework, this will only have stardew valley installations
         var entities = GameInstallMetadata.All(db);
-        entities.Count.Should().Be(4);
+        entities.Count.Should().Be(5);
         entities.Should().AllSatisfy(x => x.GameId.Value.Should().Be(GameId.From("StardewValley").Value));
     }
 }
